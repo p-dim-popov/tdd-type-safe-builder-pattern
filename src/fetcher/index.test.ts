a166aa1e8751574fde3a9af2c9e020360b9863bc
Petar Popov <p.dim.popov@gmail.com>
2022-07-31 21:30:11 +0300

    refactor fetcher tests

--- src/fetcher/index.test.ts
+++ src/fetcher/index.test.ts
@@ -1,23 +1,30 @@
-import {assertEquals, assertObjectMatch} from "https://deno.land/std@0.150.0/testing/asserts.ts";
-import {Fetcher} from "/src/fetcher/index.ts";
+import {
+  assertEquals,
+  assertObjectMatch,
+} from "https://deno.land/std@0.150.0/testing/asserts.ts";
+import { Fetcher } from "/src/fetcher/index.ts";
 import it = Deno.test;
 import {
-    assertSpyCall, assertSpyCallArg,
-    spy,
+  assertSpyCallArg,
+  spy,
 } from "https://deno.land/std@0.150.0/testing/mock.ts";
 
-it('should assign retrieved fetch and path to new object', function () {
-    const fetchSpy = spy((() => {}) as unknown as typeof fetch);
-    const fetcher = new Fetcher(fetchSpy, "/hello");
-    assertObjectMatch(fetcher, { fetch: fetchSpy, path: "/hello" })
+type Fetch = typeof fetch;
+
+const getNoopFetchMock = () => spy((() => {}) as unknown as Fetch);
+
+it("should assign retrieved fetch and path to new object", function () {
+  const fetchSpy = getNoopFetchMock();
+  const fetcher = new Fetcher(fetchSpy, "/hello");
+  assertObjectMatch(fetcher, { fetch: fetchSpy, path: "/hello" });
 });
 
 it("should have build method, returning function calling fetch with initialized path", () => {
-    const fetchSpy = spy((() => {}) as unknown as typeof fetch);
-    const service = new Fetcher(fetchSpy, "/hello").build();
+  const fetchSpy = getNoopFetchMock();
+  const service = new Fetcher(fetchSpy, "/hello").build();
 
-    assertEquals(typeof service, "function");
-    service();
+  assertEquals(typeof service, "function");
+  service();
 
-    assertSpyCallArg(fetchSpy, 0, 0, "/hello")
-})
+  assertSpyCallArg(fetchSpy, 0, 0, "/hello");
+});
