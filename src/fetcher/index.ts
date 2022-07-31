98cbd978e654336cc35437e897a45d4f18e327b0
Petar Popov <p.dim.popov@gmail.com>
2022-07-31 21:37:58 +0300

    format

--- src/fetcher/index.ts
+++ src/fetcher/index.ts
@@ -1,14 +1,14 @@
 type Fetch = typeof fetch;
 
 export class Fetcher {
-    constructor(private fetch: Fetch, private path: string) {}
+  constructor(private fetch: Fetch, private path: string) {}
 
-    build() {
-        return () => this.fetch(this.path);
-    }
+  build() {
+    return () => this.fetch(this.path);
+  }
 
-    withPath(path: string): Fetcher {
-        this.path = path;
-        return this;
-    }
+  withPath(path: string): Fetcher {
+    this.path = path;
+    return this;
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,18 @@ import readTextFile = Deno.readTextFile;
 const htmlPath = `${Deno.cwd()}/index.html`;
 
 (async function () {
-    if (await readTextFile(htmlPath).catch(() => null)) {
-        console.log(`Removing old ${htmlPath}`);
-        await remove(htmlPath)
-    }
+  if (await readTextFile(htmlPath).catch(() => null)) {
+    console.log(`Removing old ${htmlPath}`);
+    await remove(htmlPath);
+  }
 
-    const response = await fetch(`https://google.com/search?${new URLSearchParams([["q", "typescript"], ["tbs", "qdr:d"]])}`)
-    const html = await response.text();
-    await writeTextFile(htmlPath, html);
-    console.log(`${htmlPath} ready`)
-})()
+  const response = await fetch(
+    `https://google.com/search?${new URLSearchParams([["q", "typescript"], [
+      "tbs",
+      "qdr:d",
+    ]])}`,
+  );
+  const html = await response.text();
+  await writeTextFile(htmlPath, html);
+  console.log(`${htmlPath} ready`);
+})();
