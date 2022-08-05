de5d5dca419aea1953a1230bc58b6860501a0930
Petar Popov <p.dim.popov@gmail.com>
2022-08-05 22:34:33 +0300

    add fetch API with DI

--- src/functional-fetch-builder/index.test.ts
+++ src/functional-fetch-builder/index.test.ts
@@ -4,17 +4,30 @@ import {
     assertEquals,
   } from "https://deno.land/std@0.150.0/testing/asserts.ts";
 import { createFetchBuilder } from "/src/functional-fetch-builder/index.ts";
+import { getNoopFetchMock } from "/__test__/utils.ts";
+import {
+  assertSpyCall,
+} from "https://deno.land/std@0.150.0/testing/mock.ts";
 
 it("should return build function", () => {
-    const builder = createFetchBuilder({ withPath: "" });
+    const builder = createFetchBuilder({ fetch: getNoopFetchMock() }, { withPath: "" });
 
     assertEquals("build" in builder, true)
     assertEquals(typeof (builder as any).build, "function")
 });
 
+describe("build", () => {
+  it("should call fetch",  () => {
+    const fetchMock = getNoopFetchMock();
+    createFetchBuilder({ fetch: fetchMock }, { withPath: "" }).build()();
+
+    assertSpyCall(fetchMock, 0);
+  })
+})
+
 describe("withPath", () => {
   it("should assign path to builder", () => {
-    const builder = createFetchBuilder({ withPath: "/example/image.png" });
+    const builder = createFetchBuilder({ fetch: getNoopFetchMock() }, { withPath: "/example/image.png" });
 
     assertEquals("path" in builder, true);
     assertEquals((builder as any).path, "/example/image.png")
--- src/functional-fetch-builder/index.ts
+++ src/functional-fetch-builder/index.ts
@@ -1,10 +1,16 @@
+import { Fetch } from "/src/interfaces.ts";
+
+type Dependencies = {
+    fetch: Fetch;
+}
+
 type Options = {
     withPath: string;
 }
 
-export const createFetchBuilder = ({ withPath }: Options) => {
+export const createFetchBuilder = ({ fetch }: Dependencies, { withPath }: Options) => {
     return {
         path: withPath,
-        build: () => {}
+        build: () => () => fetch("")
     }
 }
